e27a72b97d93348d1d6478157f6ee2bd751b4edb
DongYoung Kim <37610908+KDongYoung@users.noreply.github.com>
2021-11-23 23:09:57 +0900

    Update DriverDrowsinessDataset_RT.py

--- DriverDrowsinessDataset_RT.py
+++ DriverDrowsinessDataset_RT.py
@@ -23,7 +23,7 @@ class EEGDataset(Dataset):
 
             return X, y, self.subj_id
         else:
-            X = self.dataset[idx][0]  # for only eeg
+            X = self.DATA[idx][0]  # for only eeg
 
 ''' dataset 생성 '''
 class DriverDrowsiness_ReactionTime():
